f522f5893c20fd252d7fffc3835d4b32e2790f5e
Bojan Milosavljević <boyan.m09@gmail.com>
2024-09-01 22:34:56 +0200

    Input bez interupcije

---
+++ other.py
@@ -0,0 +1,28 @@
+import socket
+import threading
+
+def receive_data(sock):
+    while True:
+        data, addr = sock.recvfrom(1024)
+        print(f"Primljeno od {addr}: {data.decode('utf-8')}")
+        if data.decode('utf-8') == "stop":
+            print("Prekinuto primanje podataka.")
+            break
+
+def send_data(sock, address):
+    while True:
+        user_input = input("Unesite poruku (ili 'stop' za prekid): ")
+        sock.sendto(user_input.encode('utf-8'), address)
+        if user_input == "stop":
+            print("Prekinuto slanje podataka.")
+            break
+def InputBezInterupcije():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_address = ('localhost', 12345)
+
+    recv_thread = threading.Thread(target=receive_data, args=(sock,))
+    recv_thread.start()
+
+    send_data(sock, server_address)
+
+    recv_thread.join()

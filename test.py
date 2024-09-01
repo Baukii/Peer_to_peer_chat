32bca9bf5deeff4b21f51b938b12d4c070ab31dc
Bojan Milosavljević <boyan.m09@gmail.com>
2024-09-01 22:38:41 +0200

    Klijnetski test sa inputom bez interupcije

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import socket
+from other import InputBezInterupcije
 MySocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 MySocket.connect(('10.61.1.100',33433))
 print("connected")
 MySocket.sendto(b'Hello!',('10.61.1.100',33433))
+InputBezInterupcije()

8936631b0518523b28fa5bf3679c67887c8f1c1a
Bojan Milosavljević <boyan.m09@gmail.com>
2024-09-02 12:54:18 +0200

    UDP connection na dva laptopa

--- test_server.py
+++ test_server.py
@@ -1,14 +1,20 @@
 import socket
 import threading
-from other import send_messages, receive_messages
+from connection import listen_for_client_ip, handle_client_connection
 
-ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ServerSocket.bind(('10.61.1.105', 33433))
-ServerSocket.listen()
+def start_tcp_server():
+    tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_server_socket.bind(('10.61.1.105', 33433)) 
+    tcp_server_socket.listen()
+    
+    print("TCP server is listening for incoming connections...")
 
-ConnectedSocket, addr = ServerSocket.accept()
-print("Accepted connection from " + str(addr))
+    while True:
+        client_socket, client_address = tcp_server_socket.accept()
+        print(f"Accepted connection from {client_address}")
 
-threading.Thread(target=send_messages, args=(ConnectedSocket,)).start()
-threading.Thread(target=receive_messages, args=(ConnectedSocket,)).start()
+        threading.Thread(target=handle_client_connection, args=(client_socket, client_address)).start()
 
+
+threading.Thread(target=listen_for_client_ip, daemon=True).start()
+start_tcp_server()
